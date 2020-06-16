Bugfix for metric calculation

trajopt/comanipulationpy/metrics.py
@@ -1,10 +1,7 @@
 import traj_calc
-import scene_utils
 
 import numpy as np
 import math
-
-import scene_utils
 
 import traj_utils 
 
@@ -38,7 +35,7 @@ def get_separation_dist(scene, human_pos, robot_joints, human_sphere_radius=0.05
 
     human_pos: human position from vision system - one timestep, vectorized
     the order of human_pos joints is right_shoulder + right_elbow + right_wrist + right_palm + neck + head + torso + left_shoulder + left_elbow + left_wrist + left_palm
-    robot_joint_pos: time-sampled JOINT space trajectory  - one timestep - vectorized
+    robot_joints: time-sampled JOINT space trajectory  - one timestep - vectorized
     human_sphere_radius: the radius of the spheres sampled on the human
     human_sphere_num: number of spheres per link for the human
     robot_sphere_radius: the radius of the spheres sampled on the robot
@@ -243,8 +240,8 @@ def compute_nominal_traj_metric(scene, robot_traj, nominal_traj):
     deviation = 0
     
     for i in range(num_timesteps):
-        p_eef_t = scene.get_eef_position(robot_traj[i])
-        p_nom_t = scene.get_eef_position(nominal_traj[i])
+        p_eef_t = scene.get_eef_position(list(robot_traj[i]))
+        p_nom_t = scene.get_eef_position(list(nominal_traj[i]))
 
         deviation += np.linalg.norm(p_eef_t - p_nom_t)
 